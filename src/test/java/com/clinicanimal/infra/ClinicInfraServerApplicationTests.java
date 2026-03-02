bda5f577111edf2289477bccedb4a418e3e21995
Carlos Berdejo <carlosberdejopar@gmail.com>
2026-03-02 11:54:42 -0500

    primer commit

---
+++ src/test/java/com/clinicanimal/infra/ClinicInfraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicanimal.infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicInfraServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
